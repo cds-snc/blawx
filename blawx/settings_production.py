d294b0817fc1eaed0e7af230fd43c1aeb81e7402
Sylvia McLaughlin <sylvia.mclaughlin@cds-snc.ca>
2025-09-11 16:45:31 -0700

    Making the application somewhat production ready

--- blawx/settings_production.py
+++ blawx/settings_production.py
@@ -94,22 +94,3 @@ LOGGING = {
         },
     },
 }
-
-# Email configuration for production
-if os.environ.get('EMAIL_HOST'):
-    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-    EMAIL_HOST = os.environ.get('EMAIL_HOST')
-    EMAIL_PORT = int(os.environ.get('EMAIL_PORT', 587))
-    EMAIL_USE_TLS = os.environ.get('EMAIL_USE_TLS', 'True').lower() == 'true'
-    EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
-    EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
-    DEFAULT_FROM_EMAIL = os.environ.get('DEFAULT_FROM_EMAIL', 'noreply@blawx.com')
-
-# Cache configuration for production (optional)
-if os.environ.get('REDIS_URL'):
-    CACHES = {
-        'default': {
-            'BACKEND': 'django.core.cache.backends.redis.RedisCache',
-            'LOCATION': os.environ.get('REDIS_URL'),
-        }
-    }
