48e1dfba241282d433fe939b7079394e96db96b4
Sylvia McLaughlin <sylvia.mclaughlin@cds-snc.ca>
2025-09-16 09:09:01 -0700

    Removing second egress rule for DB

--- terragrunt/aws/ecs/ecs.tf
+++ terragrunt/aws/ecs/ecs.tf
@@ -79,18 +79,6 @@ resource "aws_security_group_rule" "ecs_tasks_egress_all" {
   security_group_id = aws_security_group.ecs_tasks.id
 }
 
-# Security Group Rule: Allow outbound traffic to RDS on PostgreSQL port
-resource "aws_security_group_rule" "ecs_tasks_egress_rds" {
-  type                     = "egress"
-  from_port                = 5432
-  to_port                  = 5432
-  protocol                 = "tcp"
-  source_security_group_id = var.proxy_security_group_id
-  description              = "Allow outbound traffic to RDS PostgreSQL"
-  security_group_id        = aws_security_group.ecs_tasks.id
-}
-
-
 module "ecs" {
   source = "github.com/cds-snc/terraform-modules//ecs?ref=v10.7.0"
 
